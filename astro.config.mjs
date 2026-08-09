07150745ea8aa63a31213423c4c2e9d95f5f4518
Samuel Ball <stmball@gmail.com>
2026-08-09 08:20:14 +0100

    First Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,26 +10,12 @@ export default defineConfig({
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
+			provider: fontProviders.google(),
+			name: 'Poppins',
+			cssVariable: '--font-poppins',
 			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
+			weights: [400, 600, 700],
+			subsets: ['latin'],
 		},
 	],
 });
